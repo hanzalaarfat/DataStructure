6b8c57d89f9c12a4bde178b6203e24fbc0927205
Hanzala Arfat <hanzala735@gmail.com>
2021-06-12 23:32:16 +0530

    added selection sort

---
+++ Sort/selectionSort.js
@@ -0,0 +1,23 @@
+////// SELECTION sort /////
+
+function selectionSort(array) {
+  // Only change code below this line
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] > array[j + 1]) {
+        var temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+      }
+    }
+  }
+  console.log(array);
+  return array;
+  // Only change code above this line
+}
+
+let result = selectionSort([
+  1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92,
+]);
+console.log(result);
