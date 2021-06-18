35a220d730cfb2c94792636f1833cec7b9e5fa6c
Hanzala Arfat <hanzala735@gmail.com>
2021-06-18 22:34:52 +0530

    binary search  added

---
+++ binarySearch.js
@@ -0,0 +1,25 @@
+function binarySearch(sortedArray, key) {
+  let start = 0;
+  let end = sortedArray.length - 1;
+
+  while (start <= end) {
+    let middle = Math.floor((start + end) / 2);
+    console.log(middle);
+    if (sortedArray[middle] === key) {
+      // found the key
+
+      return middle;
+    } else if (sortedArray[middle] < key) {
+      // continue searching to the right
+      start = middle + 1;
+    } else {
+      // search searching to the left
+      end = middle - 1;
+    }
+  }
+  // key wasn't found
+  return -1;
+}
+
+let a = binarySearch([1, 4, 5, 6], 5);
+console.log(a);
